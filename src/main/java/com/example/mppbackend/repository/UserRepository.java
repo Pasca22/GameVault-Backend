338eacaf3057caf821c5f7278a2d2e0ffad48755
915-PascaAlexS <alexpasca1122@gmail.com>
2024-04-09 15:09:45 +0300

    generate random data

--- src/main/java/com/example/mppbackend/repository/UserRepository.java
+++ src/main/java/com/example/mppbackend/repository/UserRepository.java
@@ -5,22 +5,26 @@ import com.example.mppbackend.entity.User;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import com.github.javafaker.Faker;
+
 
 public class UserRepository implements Repository<User, Integer> {
 
-    List<User> users = new ArrayList<>(List.of(
-            new User(1, "Rosetta.Mante47", "ouvKJoluZ7NoCbi", "Kennedi_Fritsch16@yahoo.com",
-                    "https://avatars.githubusercontent.com/u/12451586", "122.181.149.249"),
-            new User(2, "Justina5", "H37Lk_XpNAh3tBX", "Vicenta40@hotmail.com",
-                    "https://avatars.githubusercontent.com/u/50303189", "199.91.34.103"),
-            new User(3, "Lon_Kiehn", "3sBbKWTHD4suIPQ", "Alisha67@yahoo.com",
-                    "https://avatars.githubusercontent.com/u/7457114", "142.74.30.1"),
-            new User(4, "Jabari_Conroy28", "pZ9tpSNatpNrqfN", "Gerda.Gottlieb@yahoo.com",
-                    "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/944.jpg", "118.255.70.168"),
-            new User(5, "Trinity4", "dSvo4AuNcAbRMra", "Garnett_Hermann87@yahoo.com",
-                    "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/111.jpg", "14.45.93.189"),
-            new User(6, "Alena52", "ZEwLrDpu723Wwv7", "Gianni21@yahoo.com",
-                    "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/307.jpg", "135.141.64.243")));
+    List<User> users = new ArrayList<>();
+
+    public UserRepository() {
+        Faker faker = new Faker();
+        for (int i = 1; i < 7; i++) {
+            String username = faker.name().username();
+            String password = faker.internet().password();
+            String emailAddress = faker.internet().emailAddress();
+            String avatar = faker.internet().avatar();
+            String ip = faker.internet().ipV4Address();
+
+            User newUser = new User(i, username, password, emailAddress, avatar, ip);
+            users.add(newUser);
+        }
+    }
 
     @Override
     public User getById(Integer id) {
